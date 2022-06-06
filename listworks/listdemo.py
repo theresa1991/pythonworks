6b7f2324c4e7b7c43e7a8d6ba35330a3c7bb48d1
Theresa <priyankatheresa@gmail.com>
2022-06-06 11:35:41 +0530

    list added

---
+++ listworks/listdemo.py
@@ -0,0 +1,50 @@
+#expenses=[1000,2000,3000,4000,5000]
+#for i in range(0,5):
+   # print(expenses[i])
+#for amt in expenses:
+    #print(amt)
+#name=["a","b","c","d"]
+#for i in range(0,len(name)):
+    #print(name[i])
+#for n in name:
+#    print(n)
+#num=[2,4,5,7,8]
+#for even in num:
+    #if even%2==0:
+        #print(even)
+# to print num-1 if num<15 or num+1 if num>15
+num=[12,13,14,15,16,17,18]
+#for n in num:
+    #if n>15:
+        #print(n+1)
+    #elif n<15:
+        #print(n-1)
+    #elif n==15:
+        #print(n)
+#print count of numbers where numbers in range of 14-18
+#for n in range(14,19) also correct
+#count=0
+#for n in num:
+ #   if n>=14 and n<=18:
+ #       count=count+1
+#print(count)
+#to print the count of positive negative and zero count
+num=[-1,2,3,0,-9,0,3,5,-4,6,-1]
+neg=0
+pos=0
+#zero=0
+
+#for n in num:
+   # if n<0:
+        #neg=neg+1
+    #elif n>0:
+        #pos=pos+1
+    #elif n==0:
+        #zero=zero+1
+#print(f"negative numbers count is {neg},positive count is {pos},zero count is {zero}")
+#to print the sum of numbers in list
+num=[12,13,14,15,16,17]
+sum=0
+for n in num:
+    sum+=n
+print(sum)
